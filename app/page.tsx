8e4c11c5bf6431ab5c05d7b65f701fa8df6db7a5
leon0719 <leon0970865551@gmail.com>
2025-04-24 00:04:01 +0800

    feat: init

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/company-website/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   images:{
     unoptimized: true
   },
+  basePath: "/company-website",
   trailingSlash: true,
 };
 
